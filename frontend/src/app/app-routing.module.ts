5fb8aaf9c50c0b4886c9e7a4d5b9a00d03aaf78c
SrZombra <david.gsr5@gmail.com>
2021-01-13 12:52:14 -0500

    endowment componente

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { EndowmentComponent } from './pages/novelties/endowment/endowment.component';
 import { AfterLoginService } from './token/after-login.service';
 import { BerforeLoginService } from './token/before-login.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [BerforeLoginService] },
+  { path: 'endowment', component: EndowmentComponent, canActivate: [AfterLoginService] },
   { path: '', component: HomeComponent, canActivate: [AfterLoginService] },
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { BaseUrl } from './base-url';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgIdleKeepaliveModule } from '@ng-idle/keepalive';
 import { HomeComponent } from './pages/home/home.component';
+import { EndowmentComponent } from './pages/novelties/endowment/endowment.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { HomeComponent } from './pages/home/home.component';
     NavBarComponent,
     SidenavComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    EndowmentComponent
   ],
   imports: [
     BrowserAnimationsModule,
